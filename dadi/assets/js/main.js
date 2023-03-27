27e10493c4a287935dfc3d28ae00a20fd5231f9e
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-27 16:36:43 +0200

    Preparazione cartelle e inizio esercizio Mail

---
+++ dadi/assets/js/main.js
@@ -0,0 +1,18 @@
+/*
+Mail:
+    - Chiedi all’utente la sua email,
+    - controlla che sia nella lista di chi può accedere,
+    - stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+// Chiedi all'utente la sua email
+
+// Metto un input sul DOM e lo collego a main.js
+
+// Creo la condizione attraverso la quale il value dell'input corrisponda oppure no alla lista
+
+// Quando il value dell'input corrisponde alla condizione stampo un messaggio sull'esito del controllo
+
+const input = document.getElementById(`email`).value;
+
+console.log(input)
