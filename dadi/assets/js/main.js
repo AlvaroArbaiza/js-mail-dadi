db58fdc05f921b862883fb31fac4d96915938a38
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-27 16:53:10 +0200

    Mail: Creazione lista invitati(array)

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -5,14 +5,39 @@ Mail:
     - stampa un messaggio appropriato sull’esito del controllo.
 */
 
-// Chiedi all'utente la sua email
+// 1.- Chiedi all'utente la sua email
 
-// Metto un input sul DOM e lo collego a main.js
+// 2.- Metto un input sul DOM e lo collego a main.js
 
-// Creo la condizione attraverso la quale il value dell'input corrisponda oppure no alla lista
+// 3.- Creo la condizione attraverso la quale il value dell'input corrisponda oppure no alla lista (insieme array)
 
-// Quando il value dell'input corrisponde alla condizione stampo un messaggio sull'esito del controllo
+// 4.- Quando il value dell'input corrisponde alla condizione stampo un messaggio sull'esito del controllo
 
+
+/* -------------------------------------------------------------------------------------------- */
+
+
+// Creo la variabile collegata all'input(#email) nel DOM
 const input = document.getElementById(`email`).value;
 
+// Creo l'insieme listaInvitati(array) per avere una lista di email che corrisponda al value inserito nell'input
+const listaInvitati = [
+    `alvaroarbaiza@gmail.com`,
+    `vincenzoroma@gmail.com`,
+    `luisabarone@gmail.com`,
+    `pincopallino@gmail.com`,
+    `bianconero@gmail.com`,
+    `gialloblu@gmail.com`,
+    `truefalse@gmail.com`,
+    `classeid@gmail.com`,
+    `htmlcss@gmail.com`,
+    `bootstrap@gmail.com`,
+    `javascript@gmail.com`
+];
+
 console.log(input)
+
+function myFunction() {
+
+    if ()
+}
